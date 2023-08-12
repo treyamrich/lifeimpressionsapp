0d6c8f4e3a15bf4a3101cf4423053d7348ca3899
Trey Amrich <treyamrich@gmail.com>
2023-08-12 09:17:11 -1000

    Add google signon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,10 @@
 import { baselightTheme } from "@/utils/theme/DefaultColors";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Amplify } from "aws-amplify";
+import awsExports from "../../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function RootLayout({
   children,
