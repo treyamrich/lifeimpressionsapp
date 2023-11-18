c70d04e5f744caa4d7b75eb39e079a2c2b31c71e
Trey Amrich <treyamrich@gmail.com>
2023-11-18 13:02:34 -0800

    Add unit test for field validation util functions

--- src/utils/__tests__/field-validation.test.tsx
+++ src/utils/__tests__/field-validation.test.tsx
@@ -1,7 +1,50 @@
-import { validatePhoneNumber } from "../field-validation";
+import { validateEmail, validatePhoneNumber } from "../field-validation";
 
+describe('Field Validation', () => {
+    describe('Phone Number Validation', () => {
+        it('should return trimmed values with valid phone numbers', () => {
+            const validPhoneNums = [
+                '+1 808 226 2515',
+                ' +1 323 515 1534 '
+            ];
+            validPhoneNums.forEach(x => expect(validatePhoneNumber(x)).toBe(x.trim()))
+        })
 
+        it('should fail invalid phone numbers', () => {
+            const invalidPhoneNums = [
+                '+33 808 226 2515',
+                '+1 012 515 1534'
+            ]
+            invalidPhoneNums.forEach(x => expect(validatePhoneNumber(x)).toBeUndefined())
+        })
 
-it('should pass', () => {
-    validatePhoneNumber('asdf');
+        it('should return empty string with spaces or just country code', () => {
+            const edgecases = [' ', ' +1 ', '+1'];
+            edgecases.forEach(x => expect(validatePhoneNumber(x)).toBe(""))
+        })
+    })
+
+    describe('Email Validation', () => {
+        it('should return trimmed values with valid emails', () => {
+            const validPhoneNums = [
+                'a@a.com ',
+                ' Aasdf@asdf.com '
+            ];
+            validPhoneNums.forEach(x => expect(validateEmail(x)).toBe(x.trim()))
+        })
+
+        it('should fail invalid emails', () => {
+            const invalidPhoneNums = [
+                'asdfasdf',
+                'asdf@.com',
+                'asdf.com'
+            ]
+            invalidPhoneNums.forEach(x => expect(validateEmail(x)).toBeUndefined())
+        })
+
+        it('should return empty string with empty email', () => {
+            const edgecases = [' ', '', '    '];
+            edgecases.forEach(x => expect(validateEmail(x)).toBe(""))
+        })
+    })
 })
