c92f6c7461f8f9407f19a113e6674e92851ae98a
Trey Amrich <treyamrich@gmail.com>
2024-01-30 08:23:03 -1000

    Update the way the order total is calculated

--- src/app/(DashboardLayout)/components/po-customer-order-shared-components/OrderTotalCard.tsx
+++ src/app/(DashboardLayout)/components/po-customer-order-shared-components/OrderTotalCard.tsx
@@ -15,25 +15,25 @@ const OrderTotalCard = ({ order, orderedItems }: {
     orderedItems: TShirtOrder[];
 }) => {
 
-    const totalItemCost = orderedItems
+    const subtotal = orderedItems
         .map(item => item.costPerUnit * item.quantity)
         .reduce((prev, curr) => prev + curr, 0);
+
+    const fullOrderDiscounts = order.discount;
+    const tshirtDiscounts = orderedItems
+        .map(item => item.discount)
+        .reduce((prev, curr) => prev + curr, 0);
+    const totalDiscounts = fullOrderDiscounts + tshirtDiscounts;
+
+    const discountsApplied = Math.max(0, subtotal - totalDiscounts);
 
     const taxRate = order.taxRate / 100;
     const fees = order.fees ?? 0;
     const shipping = order.shipping ?? 0;
 
-    let fullOrderDiscounts = order.discount;
-    let tshirtDiscounts = orderedItems
-        .map(item => item.discount)
-        .reduce((prev, curr) => prev + curr, 0);
-    const totalDiscounts = fullOrderDiscounts + tshirtDiscounts;
+    const tax = taxRate * discountsApplied;
 
-    let subtotal = (totalItemCost - totalDiscounts + fees + shipping);
-    subtotal = subtotal >= 0 ? subtotal : 0;
-
-    const total = subtotal * (1 + taxRate);
-    const tax = taxRate * subtotal;
+    const total = discountsApplied + tax + shipping + fees;
     const tableRowColor = '#e6e6e6';
 
     const getGridRow = (label: string, value: any, color: string = "") => (
@@ -57,7 +57,7 @@ const OrderTotalCard = ({ order, orderedItems }: {
     const formatTShirtDisplay = (tshirtOrder: TShirtOrder) => {
         const tshirt = tshirtOrder.tshirt;
         let qtyHint = ''
-        if(tshirtOrder.quantity > 1) {
+        if (tshirtOrder.quantity > 1) {
             qtyHint = ` ($${tshirtOrder.costPerUnit}) each`
         }
         return `${tshirt.styleNumber} - ${tshirt.color} ${tshirt.size} x ${tshirtOrder.quantity}${qtyHint}`
@@ -73,24 +73,24 @@ const OrderTotalCard = ({ order, orderedItems }: {
             <CardContent>
                 <Grid container rowSpacing={3}>
                     <GridSection>
-                        {getGridRow("TShirt Discounts", `$${tshirtDiscounts}`)}
-                        {getGridRow("Full Order Discounts", `$${fullOrderDiscounts}`)}
-                        {getGridRow("Total Discounts", `$${totalDiscounts}`, tableRowColor)}
-                    </GridSection>
-
-                    <GridSection>
                         {getGridRow("Total Qty", orderedItems.length)}
                         {orderedItems.map((item, i) =>
                             getGridRow(formatTShirtDisplay(item), formatTShirtOrderCost(item))
                         )}
-                        {getGridRow("Sub Total", `$${totalItemCost}`, tableRowColor)}
+                        {getGridRow("Sub Total", `$${subtotal}`, tableRowColor)}
                     </GridSection>
 
                     <GridSection>
-                        {getGridRow("Fees", `$${fees}`)}
-                        {getGridRow("Shipping", `$${shipping}`)}
+                        {getGridRow("Per Item Discounts", `-$${tshirtDiscounts}`)}
+                        {getGridRow("Full Order Discounts", `-$${fullOrderDiscounts}`)}
+                        {getGridRow("Total Discounts", `-$${totalDiscounts}`, tableRowColor)}
+                    </GridSection>
+
+                    <GridSection>
                         {getGridRow("Tax Rate", `${taxRate * 100}%`)}
                         {getGridRow("Tax", `$${tax.toFixed(2)}`)}
+                        {getGridRow("Fees", `$${fees}`)}
+                        {getGridRow("Shipping", `$${shipping}`)}
                         {getGridRow("Total", `$${total.toFixed(2)}`, tableRowColor)}
                     </GridSection>
                 </Grid>
