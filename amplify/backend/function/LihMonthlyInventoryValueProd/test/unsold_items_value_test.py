36294b4f297fcca3f4de4ede9b2d7c5d3fc45066
Trey Amrich <treyamrich@gmail.com>
2024-03-17 16:56:18 -0700

    Copy lambda changes from dev
    Commits: 0e488e6e290b0a19c49f2ab8968b92c75ae0e1e8 and 4a876eb1c2d7e0e9eae70632337002f397686ef4

--- amplify/backend/function/LihMonthlyInventoryValueProd/test/unsold_items_value_test.py
+++ amplify/backend/function/LihMonthlyInventoryValueProd/test/unsold_items_value_test.py
@@ -48,17 +48,14 @@ class TestUnsoldItemsValue(unittest.TestCase):
             (OrderType.CustomerOrder, 10, 0, 0), # i = 0; Expect 0
             (OrderType.PurchaseOrder, 3, 5.12, 100), # i = 1; Expect 0
             (OrderType.CustomerOrder, 5, 0, 0), # i = 2; Expect 0
-            (OrderType.PurchaseOrder, 24, 20.12, 10), # i = 3; Expect $(12 x 20) = $240 - $10 discount = $230
-            (OrderType.CustomerOrder, 5, 0, 0), # i = 4; Expect $(7 x 20) = $140 - $10 discount = $130
+            (OrderType.PurchaseOrder, 24, 20.12, 10), # i = 3; Expect $12(20.12 - 10/24) = $236.44
+            (OrderType.CustomerOrder, 5, 0, 0), # i = 4; Expect $7(20.12 - 10/24) = $137.92333333333335
         ])
         
         earlist_unsold_index = 3
-        remain_cost_per_unit = 20.12
-        remain_discount = 10
-        n = 7
         expected = InventoryItemValue(self.initial_cache_val.itemId)
-        expected.aggregateValue = remain_cost_per_unit * n - remain_discount + self.initial_item_value
-        expected.numUnsold = n
+        expected.aggregateValue = 137.92333333333335 + self.initial_item_value
+        expected.numUnsold = 7
         expected.earliestUnsold = datetime.fromtimestamp(earlist_unsold_index).isoformat()
         
         unsold_value = self._call_get_unsold_items_value(data)
@@ -78,16 +75,36 @@ class TestUnsoldItemsValue(unittest.TestCase):
             (OrderType.PurchaseOrder, 10, 22, 1000),
             (OrderType.CustomerOrder, -3, 33, 0),
             (OrderType.CustomerOrder, 5, 4, 0),
+            (OrderType.PurchaseOrder, 0, 22, 1000), # The discount should not affect value
             (OrderType.CustomerOrder, 5, 5, 0),
             (OrderType.CustomerOrder, 5, 1, 0),
         ])
-        earlist_unsold_index = 4
+        earlist_unsold_index = 5
         n = -2
         expected = InventoryItemValue(self.initial_cache_val.itemId)
         expected.aggregateValue = self.initial_item_value
         expected.numUnsold = n
         expected.earliestUnsold = datetime.fromtimestamp(earlist_unsold_index).isoformat()
 
+        unsold_value = self._call_get_unsold_items_value(data)
+        self.assertEqual(expected, unsold_value)
+
+    def test_any_adjustments(self):
+        data = self._transform_data([
+            (OrderType.CustomerOrder, 3, 0, 0),  # i = 0; Sold 3; Expect: $0
+            (OrderType.CustomerOrder, -3, 0, 0),  # i = 1; Returned 3; Expect: $0
+            (OrderType.PurchaseOrder, 5, 10, 5),  # i = 2; Bought 5; Expect: $(5 x 10 - 5) = $45
+            (OrderType.PurchaseOrder, -1, 0, 0),  # i = 3; Damaged items 1; Expect $4(10 - 5/5) = $36
+            (OrderType.PurchaseOrder, 2, 20, 10),  # i = 4; Bought 2; Expect: $4(10 - 1) + 2(20 - 10/2) = $66
+            (OrderType.CustomerOrder, 3, 0, 0), # i = 5; Sold 3; Expect $1(10 - 1) + 2(20 - 10/2) = $39
+        ])
+        earlist_unsold_index = 2
+        n = 3
+        expected = InventoryItemValue(self.initial_cache_val.itemId)
+        expected.aggregateValue = 39 + self.initial_cache_val.aggregateValue
+        expected.numUnsold = n
+        expected.earliestUnsold = datetime.fromtimestamp(earlist_unsold_index).isoformat()
+        
         unsold_value = self._call_get_unsold_items_value(data)
         self.assertEqual(expected, unsold_value)
 
